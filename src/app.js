a732d8db3969169a2f0673054a29fd2493e36d4e
David Seidenberg <davidseidenberg@gmail.com>
2018-03-31 11:23:02 -0700

    Build top level component App

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const title = 'Creator\'s Block App';
+
+
+const App = (props) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
 
-const title = 'My Minimal React Webpack Babel Setup';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('app')
 );
