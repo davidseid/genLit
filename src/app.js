5024a244197770b44ee551833d9d946bf7ce1bd0
David Seidenberg <davidseidenberg@gmail.com>
2018-03-31 12:35:32 -0700

    Set state of app to source object from source input

--- src/app.js
+++ src/app.js
@@ -10,12 +10,38 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      source: {example: 'ex'}
+      source: {}
     }
   }
 
   setSourceText(text) {
-    console.log(text);
+    // output: object with 
+      //each word as a prop
+      //and an array of next words
+      //and a count
+      //and a prop of the most common word
+
+    const source = {};
+    const words = text.split(' ');
+    
+    words.forEach((word, index) => {
+      word = word.toLowerCase();
+      if (word[word.length -1 ] === '.' || 
+          word[word.length -1 ] === ';' ||
+          word[word.length -1 ] === ',') {
+            word = word.slice(0, word.length - 1);
+          }
+      if (!source[word]) {
+        source[word] = {count: 1, next: [words[index + 1]]};
+      } else {
+        source[word].count += 1;
+        source[word].next.push(words[index + 1]);
+      }
+    });
+
+    this.setState({
+      source: source
+    });
   }
 
 
